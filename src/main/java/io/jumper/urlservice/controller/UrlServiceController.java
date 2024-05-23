3b9b78085c653ebcc23ee68ff8a5ed1111c6bf00
Simon Oser <simon.oser@gmail.com>
2024-05-23 15:04:58 +0200

    Use log.debug for log-messages

--- src/main/java/io/jumper/urlservice/controller/UrlServiceController.java
+++ src/main/java/io/jumper/urlservice/controller/UrlServiceController.java
@@ -23,7 +23,7 @@ public class UrlServiceController {
         log.debug("'GET " + SERVICE_API_V1 + "/" + shortUrl);
         var url = urlService.getLongUrl(shortUrl);
         if (url.isEmpty()) {
-            log.info("-> NOT Found");
+            log.debug("-> NOT Found");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
         log.debug("-> originalPath: " + url.get().getLongUrl());
@@ -34,7 +34,7 @@ public class UrlServiceController {
     public ResponseEntity<UrlData> create(@RequestBody UrlData url) {
         var savedUrl = urlService.saveUrl(url.getShortUrl(), url.getLongUrl(),url.getUserid());
         if (savedUrl.isEmpty()) {
-            log.info("Url not created:" + url.getShortUrl());
+            log.debug("Url not created:" + url.getShortUrl());
             throw new UrlServiceException("Url not created");
         }
         return new ResponseEntity<>(savedUrl.get(), HttpStatus.CREATED);
